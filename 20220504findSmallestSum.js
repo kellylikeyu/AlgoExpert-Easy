90446a45d6b7759dc1b3f63389246d6f2ce4bf4a
kellylikeyu <100189509+kellylikeyu@users.noreply.github.com>
2022-05-04 16:27:32 -0700

    Create 20220504findSmallestSum.js

---
+++ 20220504findSmallestSum.js
@@ -0,0 +1,17 @@
+function nonConstructibleChange(coins) {
+  // Write your code here.
+	if(coins.length ===0) {
+		return 1;
+	}
+	let sortedCoins = coins.sort((a,b) => a-b);
+	let sum = 0;
+	for(let i=0; i<coins.length; i++) {
+		if(sortedCoins[i] - sum > 1) {
+			return sum+1;
+		}
+		sum += sortedCoins[i];
+	}return sum+1;
+}
+
+// Do not edit the line below.
+exports.nonConstructibleChange = nonConstructibleChange;
